90fb78e117ce652c1ca7727f2ac122ce5e28638e
Otavio Pace <otaviopp8@gmail.com>
2021-02-25 00:20:16 -0300

    product: Connect service to database with models, migrations and seeders

--- product/src/bin/index.js
+++ product/src/bin/index.js
@@ -1,11 +1,15 @@
 const { startHttpServer } = require('js-commons/src/ports/http')
+const repository = require('js-commons/src/ports/repository')
 const setupDotenv = require('js-commons/src/config')
 const { logger } = require('js-commons/src/logger')
 const { createApp } = require('../app')
+const models = require('../ports/sequelize/models')
 
 setupDotenv()
 
 const main = async () => {
+  const repo = await repository.connect(models)
+
   const app = createApp()
 
   startHttpServer(app)
---
+++ product/src/ports/sequelize/models/product.js
@@ -0,0 +1,27 @@
+const create = (sequelize, { INTEGER, STRING }) =>
+  sequelize.define('Product', {
+    id: {
+      type: STRING,
+      primaryKey: true,
+      allowNull: false
+    },
+    price: {
+      type: INTEGER,
+      allowNull: false,
+      required: true
+    },
+    title: {
+      type: STRING,
+      allowNull: false,
+      required: true
+    },
+    description: {
+      type: STRING,
+      allowNull: false,
+      required: true
+    }
+  })
+
+module.exports = {
+  create
+}
